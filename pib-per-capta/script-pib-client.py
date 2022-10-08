ff5cfb0f729aeb9c96ae989ad00aa4df338b4e6c
Joice <joicerodrigues020@gmail.com>
2022-10-07 21:09:48 -0300

    método para retornar estados/regiões

--- pib-per-capta/script-pib-client.py
+++ pib-per-capta/script-pib-client.py
@@ -8,6 +8,15 @@ def main():
     calculate.load_file()
     calculate.load_uf_by_region()
     calculate.print_all_content()
-    
+    calculate.get_state_by_region('CO')
+    calculate.get_region_by_state('Alagoas')
+    calculate.state_for('CO')
+    calculate.region_for('Amapa')
+    calculate.data_frame_for_state('CO')
+    calculate.data_frame_for_region('Goias')
+    calculate.tot_state()
+    calculate.tot_region()
+    calculate.first_region()
+    calculate.last_region()
 
 main()
--- pib-per-capta/singleton/calculate_pib_per_capta_singleton.py
+++ pib-per-capta/singleton/calculate_pib_per_capta_singleton.py
@@ -1,45 +1,58 @@
 import pandas as pd
-from abc import ABC, abstractmethod
+from abc import ABC, abstractclassmethod
 
-# Qual classe é a abstração e qual é a implementação?
-# R: A classe abstrata é AbstractCalculatePib e a a implementação é CalculatePibPerCaptaSingleton
-
-# Implementar a classe abstrata completa CalculatePibPerCaptaSingleton
-# Fazer o diagrama UML d PIB PerCapta
-
+#Implementar a abstração completa CalculatePibPerCapta
+#Diagrama UML CalculatePibPerCapta
 
 class Config:
     _base_file = "data/base.xlsx"
-    _region = "UF_Regiao"
+    _region    = "UF_Regiao"
 
-
-class AbstractCalculatePib(ABC):
-    @abstractmethod
+#Uma classe abstrata representa um contrato com regras, com regras, assinaturas e tipo de retorno
+#No exemplo abaixo AbstractCalculaPib
+#Quantos contratos temos: 1
+#Quantas regras temos: 4
+#Quantas assinaturas: 4
+#Como podemos violar o contrato de AbstractCalculaPib: Não implementando todos os metodos da classe
+#Obriga a utilizar o metodo no código
+class AbstractCalculaPib(ABC):
+    @abstractclassmethod
     def get_instance():
-        raise RuntimeError("TODO: Método 'get_instance' ainda não está implementado")
+        raise RuntimeError('Metodo ainda não implementado')
 
-    def load_file():
-        raise RuntimeError("TODO: Método 'load_file' ainda não está implementado")
+    @abstractclassmethod
+    def load_file(self):
+        #Caso não seja implementado abaixo
+        raise RuntimeError('Metodo ainda não implementado')
 
-    def load_uf_by_region():
-        raise RuntimeError("TODO: Método 'load_uf_by_region' ainda não está implementado")
+    @abstractclassmethod
+    def load_uf_by_region(self):
+        #Caso não seja implementado abaixo
+        raise RuntimeError('Metodo ainda não implementado')
 
-    def print_all_content():
-        raise RuntimeError("TODO: Método 'print_all_content' ainda não está implementado")
+    @abstractclassmethod
+    def print_all_content(self):
+        #Caso não seja implementado abaixo
+        raise RuntimeError('Metodo ainda não implementado')
 
-
-class CalculatePibPerCaptaSingleton(AbstractCalculatePib):
-    # atributos da classe
+    
+    @abstractclassmethod
+    def get_state_by_region(self, state):
+        #Caso não seja implementado abaixo
+        raise RuntimeError('Metodo ainda não implementado')
+    
+class CalculatePibPerCaptaSingleton(AbstractCalculaPib):
+    # atributos da classe 
     _instance = None
-    raw_data = None
+    raw_data  = None
     current_content = None
+
 
     # Métodos
 
     # Construtor da classe
-
     def __init__(self):
-        # Estamos lançando uma exception em
+        # Estamos lançando uma exception em 
         # tempo de execução
         raise RuntimeError('Singleton!!')
 
@@ -52,14 +65,67 @@ class CalculatePibPerCaptaSingleton(AbstractCalculatePib):
         else:
             return cls._instance
 
+    @classmethod
     def load_file(self):
         print("Inicio do script de PIB x Percapta")
         self.raw_data = pd.ExcelFile(Config._base_file)
         return self.raw_data
 
+    @classmethod
     def load_uf_by_region(self):
-        self.current_content = pd.read_excel(self.raw_data, Config._region)
+        self.current_content = pd.read_excel(self.raw_data, Config._region )
         return self.current_content
 
+    @classmethod
     def print_all_content(self):
         print(self.current_content)
+
+    
+    def get_state_by_region(self, state):
+        satates = self.current_content[self.current_content['Regiao'] == state]
+        print(satates)
+
+    def get_region_by_state(self, state):
+        region = self.current_content[self.current_content['Estado'] == state]
+        print(region)
+
+    def state_for(self, state):
+        lista = []
+        for index, row in self.current_content.iterrows():
+            if (row['Regiao'] == state):
+                lista.append(row['Estado'])
+        print(lista)
+
+    def region_for(self, state):
+        lista2 = []
+        for index, row in self.current_content.iterrows():
+            if (row['Estado'] == state):
+                lista2.append(row['Regiao'])
+        print(lista2)
+
+    def data_frame_for_state(self, state):
+        for c, row in self.current_content.iterrows():
+            if (row['Regiao'] == state):
+                print(self.current_content.loc[c:c])
+
+    def data_frame_for_region(self, state):
+        for c, row in self.current_content.iterrows():
+            if (row['Estado'] == state):
+                print(self.current_content.loc[c:c])
+
+    def tot_state(self):
+        for c, row in self.current_content.iterrows():
+            tot = c + 1
+        print('Total de Estados:',tot)
+
+    def tot_region(self):
+        tot = self.current_content['Regiao'].value_counts()
+        print('Total de regiões e estados por regiões\n',tot)
+
+    def first_region(self):
+        first = self.current_content[['Regiao', 'Estado']].loc[0:0]
+        print(first)
+
+    def last_region(self):
+        last = self.current_content[['Regiao', 'Estado']].loc[26:26]
+        print(last)
